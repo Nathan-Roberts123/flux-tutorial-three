149581369c198febfeea1bc3afe587f0b7b25c5b
Nathan-Roberts123 <robertsnathan2004@gmail.com>
2026-07-01 12:45:18 +0200

    cluster_name output

--- terraform/output.tf
+++ terraform/output.tf
@@ -8,3 +8,8 @@ output "flux_namespace" {
   description = "Namespace where Flux is installed"
   value       = flux_bootstrap_git.this.namespace
 }
+
+output "eks_cluster_name" {
+  description = "Name of an EKS Cluster that was created"
+  value       = module.eks_al2023.cluster_name
+}
